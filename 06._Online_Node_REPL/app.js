2bed580f90b298781122d0934ab07e86d52cf3b3
Joakim Rosdahl Severinsen <jose0001@stud.ek.dk>
2026-03-05 08:29:37 +0100

    Begyndt på Online Node.js REPL projektet.

--- 06._Online_Node_REPL/app.js
+++ 06._Online_Node_REPL/app.js
@@ -2,7 +2,30 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.static('./public'));
+app.use(express.json());
+import path from 'path';
 
+// ======================= Pages =======================
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve('public/frontpage/frontpage.html'));
+});
+
+// ======================= API =======================
+
+app.post('/api/repl', (req, res) => {
+    // Optional chaining of req.body to return undefined if req.body is that. Avoids crash if we look for replCode in undefined
+    let replCode = req.body?.replCode;
+
+    if(!replCode) {
+        res.status(400).send({ errorMessage: 'Missing the key replCode in the JSON body' });
+    }
+
+    replCode = replCode.replace('console.log("', ''). replace('")');
+
+    res.send({ data: replCode });
+});
 
 app.listen(8080, () => {
     console.log('Server is listening on port', 8080);
---
+++ 06._Online_Node_REPL/design.md
@@ -0,0 +1,7 @@
+# Pages
+## Frontpage with the product itself. No login, no landing page
+
+## About
+Description of the people behind the application
+
+## Contact
